8669a88210750e5f1565b94ca50a97633f1789f5
stinek4 <54266608+stinek4@users.noreply.github.com>
2021-04-05 17:07:12 +0200

    Favorites
    xml

--- app/src/main/java/com/example/cocktail_app/Favorites.kt
+++ app/src/main/java/com/example/cocktail_app/Favorites.kt
@@ -1,16 +1,21 @@
 package com.example.cocktail_app
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cocktail_app.ui.LibraryMainViewModel
 import kotlinx.android.synthetic.main.fragment_favorites.*
 
 
 class Favorites : Fragment() {
     var myAdapter: MyAdapter? = null
+    val myDrinks = listOf<Cocktail>()
+
 
     companion object {
         fun newInstance() = Favorites()
@@ -20,6 +25,7 @@ class Favorites : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        favoritesViewModel = ViewModelProvider(this).get(LibraryMainViewModel::class.java)
         return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 
@@ -43,5 +49,7 @@ class Favorites : Fragment() {
     fun updateList()
 
 }*/
+
+
 
 }
--- app/src/main/java/com/example/cocktail_app/ui/Library.kt
+++ app/src/main/java/com/example/cocktail_app/ui/Library.kt
@@ -1,5 +1,4 @@
 package com.example.cocktail_app.ui
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,85 +17,57 @@ import kotlinx.android.synthetic.main.fragment_library.*
 
 
 class Library : Fragment() {
-
     private lateinit var cocktails: List<Cocktail>
-
     //Adapter, and greier for display av info om boligene. Håndtering av RecyclerView layouten
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var cocktailAdapter: MyAdapter
-
     //MainViewModel
     private lateinit var libraryViewModel : LibraryMainViewModel
     private val model: LibraryMainViewModel by activityViewModels()
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         //Create viewmodel
         libraryViewModel = ViewModelProvider(this).get(LibraryMainViewModel::class.java)
-
         return inflater.inflate(R.layout.fragment_library, container, false)
     }
-
-
     //LIM INN LIST OF COCKTAILS I LINJE 31
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //displayCocktails()
         getAllCocktails()
     }
-
-
-
     //METHODS
-
     private fun getAllCocktails(){
-
-            libraryViewModel.getCocktailsAPI(
-                Volley.newRequestQueue(context),
-
-                    //IF Kall is successfull, get Cocktails.
-                { getCocktails ->
-                    displayCocktails(getCocktails.cocktailList)
-                    //cocktailAdapter.updateData(getCocktails)
-                    //scrollToBottom()
-                },
-                {
-                   //Noe gikk galt...
-                }
-            )
-
+        libraryViewModel.getCocktailsAPI(
+            Volley.newRequestQueue(context),
+            //IF Kall is successfull, get Cocktails.
+            { getCocktails ->
+                displayCocktails(getCocktails.cocktailList)
+                //cocktailAdapter.updateData(getCocktails)
+                //scrollToBottom()
+            },
+            {
+                //Noe gikk galt...
+            }
+        )
     }
-
-
-
     private fun displayCocktails(cocktails: List<Cocktail>){
-
         val cocktailsList = listOf<String>("Martini","Gintonic","asd","asd","liuasd","lasduo","lol")
         viewManager = LinearLayoutManager(requireContext())
         viewAdapter = MyAdapter(cocktails)  //oppretter en objekt av MyAdapter klassen min. Parameter en mutablelist.
-
-
         recyclerView = cocktail_recyclerview.apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
             setHasFixedSize(true)
-
             // use a linear layout manager
             layoutManager = viewManager
-
             // specify an viewAdapter (see also next example)
             //adapter = viewAdapter
             cocktail_recyclerview.adapter = viewAdapter
         }
     }
-
-    //TODO: Adapter - FINISH
-    //TODO: Recyclerview - FINISH
-    //TODO: Hent in API after its all clear & Display on Recyclerview - FINISH
-
 }
