1bdc7a8d1a8f155eb6df5e7060a7390d9effdc14
RheaM <rheamarie-@live.no>
2021-04-05 21:01:43 +0200

    - last version

--- app/src/main/java/com/example/cocktail_app/Random.kt
+++ app/src/main/java/com/example/cocktail_app/Random.kt
@@ -1,6 +1,7 @@
 package com.example.cocktail_app
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -36,6 +37,7 @@ class Random : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        generator_btn.setBackgroundColor(Color.CYAN);
         generator_btn.setOnClickListener {
             val intent = Intent(context, Random2::class.java)
             startActivity(intent);
